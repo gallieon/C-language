5524e0d6fcdeb7d5297cdeb4adffd820cbd2fead
gallieon <95416064+gallieon@users.noreply.github.com>
2022-01-04 04:28:42 -0800

    help yourself

---
+++ asmd(add,subtraction,multiplication,division).c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    int add,sub,mul,dive,num1,num2;
+    printf("enter first number\n");
+    scanf("%d",&num1);
+    printf("enter second number\n");
+    scanf("%d",&num2);
+    printf("addition: %d\n",num1+num2,add);
+    printf("subtraction: %d\n",num1-num2,sub);
+    printf("multiply: %d\n",num1*num2,mul);
+    printf("division: %d\n",num1/num2,dive);
+
+    
+}
+
