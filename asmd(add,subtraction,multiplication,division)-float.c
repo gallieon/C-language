26c39a0392ace042aff5389bd73f4ed218d7345b
gallieon <95416064+gallieon@users.noreply.github.com>
2022-01-04 04:29:56 -0800

    asmd-float

---
+++ asmd(add,subtraction,multiplication,division)-float.c
@@ -0,0 +1,21 @@
+ 
+#include <stdio.h>
+
+int main()
+{
+    float add,sub,mul,dive,num1,num2;
+    printf("enter first number\n");
+    scanf("%f",&num1);
+    printf("enter second number\n");
+    scanf("%f",&num2);
+    
+    printf("addition: %f\n",num1+num2,add);
+    printf("subtraction: %f\n",num1-num2,sub);
+    printf("multiplication: %f\n",num1*num2,mul);
+    printf("division: %f\n",num1/num2,dive);
+    getch();
+    
+ 
+}
+
+
