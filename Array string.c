440d4fafeb2c5edaf5e238a49ad17110eedb2ae8
gallieon <95416064+gallieon@users.noreply.github.com>
2022-01-13 22:11:37 +0530

    Array string

---
+++ Array string.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    char ch[300]={'g','a','l','l','i','e','o','n','\0'};
+    char ch2[300]="gallieon";
+    
+    printf("the charactors of your string: %s\n",ch);
+    printf("litral charactors of your string: %s\n",ch2);
+}
