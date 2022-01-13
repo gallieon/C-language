4730fe26ef7d3efb6ed1ad0981160cb9cf3d3cd8
gallieon <95416064+gallieon@users.noreply.github.com>
2022-01-13 21:55:29 +0530

    ASCII

---
+++ ASCII Value.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    char s;
+    printf("\n enter a num ber for finding ASCII value:\n");
+    scanf("%c",&s);
+    printf("your ASCII value of %c is %d",s,s);
+}
+
