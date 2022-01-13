98c9c762976c74f7ea4934ddc0e758913e44d2ee
gallieon <95416064+gallieon@users.noreply.github.com>
2022-01-13 22:26:18 +0530

    switch

---
+++ Switch.c
@@ -0,0 +1,20 @@
+#include<stdio.h>  
+void main(){    
+int number=0;     
+printf("enter a number:");    
+scanf("%d",&number);    
+switch(number){    
+case 10:    
+printf("enterd number is equals to 10");    
+break;    
+case 50:    
+printf("enterd number is equal to 50");    
+break;    
+case 100:    
+printf("enterd number is equal to 100");    
+break;    
+default:    
+printf("enterd number is not equal to 10, 50 or 100");    
+}    
+ 
+}   
