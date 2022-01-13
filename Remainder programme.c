ff15a45899fdbfbba098ce9bbe333d58b126b662
gallieon <95416064+gallieon@users.noreply.github.com>
2022-01-13 22:04:25 +0530

    Remainder

---
+++ Remainder programme.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    int z,a=5,b=6;
+    z=b*a;
+    printf("the value of z is %d\n",z);
+    printf("5 is when divided by 2 leaves the remainder of %d\n",5%2);
+    printf("5 is when divided by 2 leaves the remainder of %d\n",-5%2);
+    printf("5 is when divided by 2 leaves the remainder of %d\n",5%-2);
+    
+    getch();
+        
+}
+
