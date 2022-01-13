99da63b9a5364fa9029130abf09bac721ca12033
gallieon <95416064+gallieon@users.noreply.github.com>
2022-01-13 21:58:40 +0530

    fibonacci series

---
+++ Fibonacci Series.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    int n1=0,n2=1,n3,i,number;
+    printf("enter an integer \n");
+    scanf("%d",&number);
+    printf("\n%d %d",n1,n2);
+    for(i=2;i<number;i++)
+    {
+        n3=n1+n2;
+        printf(" %d",n3);
+        n1=n2;
+        n2=n3;
+        
+    }
+    getch();
+}
+
