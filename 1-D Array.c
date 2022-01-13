4a4ed79642d06a4b5c6da2883e90aa636f53b717
gallieon <95416064+gallieon@users.noreply.github.com>
2022-01-13 22:05:21 +0530

    1-D array

---
+++ 1-D Array.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    int arr[5];
+    printf("enter 5 value\n");
+    scanf("%d %d %d %d %d",&arr[0],&arr[1],&arr[2],&arr[3],&arr[4]);
+    printf("%d %d %d %d %d",arr[0],arr[1],arr[2],arr[3],arr[4]);
+}
