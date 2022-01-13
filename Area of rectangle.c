231d20d48fb9583a7af7d0d986c9a34d1898753b
gallieon <95416064+gallieon@users.noreply.github.com>
2022-01-13 21:19:14 +0530

    area of rectangle

---
+++ Area of rectangle.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    float length,width,aor;
+    printf("enter the length of rectangle\n");
+    scanf("%f\n",&length);
+    printf("enter the width of the rectangle\n");
+    scanf("%f\n",&width);
+    printf("Area of rectangle: %f",length*width,aor);
+    getch();
+}    
