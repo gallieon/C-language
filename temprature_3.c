bc9a60b729facdc1392ffbaafc1990b494cf90a5
gallieon <95416064+gallieon@users.noreply.github.com>
2022-08-26 22:51:56 +0530

    Create temprature_3.c
    Convert Fahrenheit to Kelvin

---
+++ temprature_3.c
@@ -0,0 +1,16 @@
+// C Program to Convert Fahrenheit to Kelvin
+#include <stdio.h>
+int main(){
+    float F, kel;
+    
+    // Taking input
+    printf("Enter the temperature in Fahrenheit: ");
+    scanf("%f", &F);
+    
+    // Fahrenheit to kelvin conversion 
+    kel = ((5.0 / 9) * (F - 32)) + 273.15;
+    
+    // Display output
+    printf("%.2f degree Fahrenheit = %.2f Kelvin", F, kel);
+    return 0;
+}
